fe360e61b2be45a47e321392fb23165eeeebb61e
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-09 10:00:12 -0500

    feat: add division by zero handling

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+    if (num2 === 0) {
+        return "You can't divide by zero!"
+    }
+    else {
+        return Math.round((num1 / num2) * 10)/10
+    }
 }
 
 /**
